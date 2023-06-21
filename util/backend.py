5006aa77818c05a813415ee541bbe963ff8dc77d
Lana Miličević <lana.milicevic10@gmail.com>
2023-06-21 09:36:05 +0200

    Fix room list type error when adding new rooms

--- util/backend.py
+++ util/backend.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
 
 
@@ -12,7 +14,7 @@ class Backend(QObject):
 
     def load_room_list(self):
         try:
-            with open('../room_list.json', 'r') as file:
+            with open('./room_list.json', 'r') as file:
                 room_json = json.load(file)
                 self.room_list = [room['name'] for room in room_json]
                 print("Loaded room names:", self.room_list)
@@ -23,22 +25,37 @@ class Backend(QObject):
         return self.room_list
 
     def save_room_list(self):
-        with open('../room_list.json', 'w') as file:
+        with open('./room_list.json', 'w+') as file:
             json.dump(self.room_list, file)
 
     @pyqtSlot(str)
     def addRoom(self, room_name):
-        next_id = 0
-        if self.room_list:
-            existing_ids = [room['id'] for room in self.room_list]
-            next_id = max(existing_ids) + 1
+        json_file = 'room_list.json'
+        if not os.path.exists(json_file):
+            # Create an empty room list if the file doesn't exist
+            with open(json_file, 'w') as file:
+                json.dump([], file)
 
+        # Load existing room list from JSON file
+        with open(json_file, 'r') as file:
+            existing_rooms = json.load(file)
+
+        # Find the maximum ID in the existing rooms
+        existing_ids = [room['id'] for room in existing_rooms]
+        next_id = max(existing_ids) + 1 if existing_ids else 0
+
+        # Create a new room dictionary
         room = {
             'id': next_id,
             'name': room_name
         }
-        self.room_list.append(room)
-        self.save_room_list()
+
+        existing_rooms.append(room)
+
+        with open(json_file, 'w') as file:
+            json.dump(existing_rooms, file)
+
+        self.room_list = [room['name'] for room in existing_rooms]
 
     @pyqtSlot(result=list)
     def loadData(self):
