183966dd159e5b99822dc6a569adc5afcb6d02e3
Lana Miličević <lana.milicevic10@gmail.com>
2023-08-14 14:50:52 +0200

    Make generated values realistic

--- util/serial_thread.py
+++ util/serial_thread.py
@@ -3,6 +3,11 @@ from PyQt5.QtCore import QThread, pyqtSignal
 import random
 import time
 
+GEN_TEMPERATURE_RANGE = (-60, 110)
+GEN_PERCENTAGE_RANGE = (-10, 110)
+GEN_PRESSURE_RANGE = (800, 1200)
+GEN_BRIGHTNESS_RANGE = (-10, 1100)
+
 
 def generate_dummy_data(backend):
     measurements = ['T', 'H', 'P', 'B', 'C']
@@ -15,7 +20,17 @@ def generate_dummy_data(backend):
 
         room_id = random.choice(room_ids)
         measurement = random.choice(measurements)
-        value = random.uniform(0, 1000)         # FIXME: realistic ranges
+        
+         # Generate a value that sometimes falls out of the valid range defined in logger
+        if measurement == 'T':
+            value = random.uniform(GEN_TEMPERATURE_RANGE[0], GEN_TEMPERATURE_RANGE[1])
+        elif measurement == 'H' or measurement == 'C':
+            value = random.uniform(GEN_PERCENTAGE_RANGE[0], GEN_PERCENTAGE_RANGE[1])
+        elif measurement == 'P':
+            value = random.uniform(GEN_PRESSURE_RANGE[0], GEN_PRESSURE_RANGE[1])
+        elif measurement == 'B':
+            value = random.uniform(GEN_BRIGHTNESS_RANGE[0], GEN_BRIGHTNESS_RANGE[1])
+
 
         message = f"{room_id} {measurement} {value:.2f}"
         print("Generated message:", message)
